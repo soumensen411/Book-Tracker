a9a26a99896cb477f9c49f5bfa518d327ffc0ef0
soumen_sen <144879914+soumensen411@users.noreply.github.com>
2026-03-18 14:06:56 +0000

    Configure for GitHub Pages deployment

--- Book-Tracker/vite.config.js
+++ Book-Tracker/vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  base: '/<YOUR-REPO-NAME>/', // e.g., '/Book-Tracker/'
+  plugins: [react()],
 })
