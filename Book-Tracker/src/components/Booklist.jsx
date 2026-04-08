1dbe93b1d43b222a877c55c98779a6cac86f049c
soumen_sen <144879914+soumensen411@users.noreply.github.com>
2026-04-08 13:41:01 +0000

    same some ui stuff and fix bug

--- Book-Tracker/src/components/Booklist.jsx
+++ Book-Tracker/src/components/Booklist.jsx
@@ -2,21 +2,21 @@ import React from "react";
 
 const Booklist = ({ books, onDel, onStatus }) => {
   return (
-    <div className="w-full flex justify-center">
-      <div className="w-full md:w-3/4 lg:w-1/2">
+    <div className="w-full flex justify-center ">
+      <div className="w-full md:w-3/4 lg:w-1/2 ">
         <ul className="list bg-base-100 rounded-box  shadow-md ">
           {books.map((book, index) => (
             <li
               key={book.id}
-              className="list-row flex flex-col sm:flex-row gap-4 p-3 mt-2 border border-neutral-500 items-center"
+              className="rounded-lg list-row flex flex-col sm:flex-row gap-4 p-3 mt-2 border border-neutral-500 items-center"
             >
-              <div className="text-4xl font-thin opacity-30 tabular-nums">
+              <div className="text-4xl font-thin opacity-30 tabular-nums ">
                 {`${index<9 ? `0${index + 1}` : index+1}`}
 
               </div>
               <div>
                 <img
-                  className="size-15 rounded-box"
+                  className="size-15 rounded-box rounded-lg object-cover"
                   src="https://as2.ftcdn.net/v2/jpg/05/97/47/95/1000_F_597479556_7bbQ7t4Z8k3xbAloHFHVdZIizWK1PdOo.jpg"
                 />
               </div>
@@ -27,7 +27,7 @@ const Booklist = ({ books, onDel, onStatus }) => {
                 </div>
 
                 <span
-                  className={`badge text-xs font-semibold px-2 py-1 rounded-full ${
+                  className={`badge text-xs font-semibold px-2 py-1 rounded-full  ${
                     book.status === "want to read"
                       ? "bg-blue-100 text-blue-700"
                       : book.status === "reading"
@@ -45,7 +45,7 @@ const Booklist = ({ books, onDel, onStatus }) => {
               <div className="flex flex-wrap justify-center gap-2">
                 <button
                   onClick={() => onStatus(book.id)}
-                  className="btn bg-transparent border border-neutral-500"
+                  className="btn bg-transparent border border-neutral-500 rounded-lg"
                 >
                   {book.status === "want to read" && "Start Reading"}
                   {book.status === "reading" && "Mark Completed"}
@@ -53,7 +53,7 @@ const Booklist = ({ books, onDel, onStatus }) => {
                 </button>
                 <button
                   onClick={() => onDel(book.id)}
-                  className="font-thin  btn btn-square btn-ghost items-center border border-neutral-500"
+                  className="rounded-lg font-thin  btn btn-square btn-ghost items-center border border-neutral-500"
                 >
                   ❌
                 </button>
